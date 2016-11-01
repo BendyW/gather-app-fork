c86a9e92e379b2307a5d5ff624a9a2300f83dbda
Arnold Rozon <arnold@rozon.org>
2016-11-01 10:45:07 -0500

    changes to main layout

---
+++ public/scripts/main.js
@@ -0,0 +1,10 @@
+//Vanilla JS stuff
+// var mainBody = document.querySelector('#main');
+// var mapBody = document.querySelector('#map-container');
+
+var mainBody = $('#main');
+var mapBody = $('#map-container');
+
+function hideAndShow(obj){
+    $('#main')
+}
